dba378755339581f4a42c267688646c97df3f257
ChoiHyungKeun <gudrms1592@naver.com>
2022-08-05 00:07:12 +0900

    입력창 만들기

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import AddToDo from './components/AddToDo';
 import DateHead from './components/DateHead';
+import Empty from './components/Empty';
 
 function App() {
   const today = new Date();
 
   return (
-    <SafeAreaView>
-      <DateHead date={today} />
-      <AddToDo />
+    <SafeAreaView style={styles.block}>
+      <KeyboardAvoidingView
+        behavior={Platform.select({ios: 'padding', android: undefined})}
+        style={styles.avoid}>
+        <DateHead date={today} />
+        <Empty />
+        <AddToDo />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
+  },
+});
 
 export default App;
--- components/AddToDo.js
+++ components/AddToDo.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
 
 function AddToDo() {
-  return <View style={styles.block} />;
+  const [text, setText] = useState('');
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할일을 입력하세요"
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdb',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 8,
   },
 });
 
