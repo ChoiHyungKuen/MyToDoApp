be1a2247d36a1b5626133f7d987619ddc4e8e063
ChoiHyungKeun <gudrms1592@naver.com>
2022-08-08 23:13:12 +0900

    제목 수정

--- components/DateHead.js
+++ components/DateHead.js
@@ -11,7 +11,7 @@ function DateHead({date}) {
       <StatusBar backgroundColor="#26a69a" />
       <View style={styles.block}>
         <Text style={styles.dateText}>
-          {year}년 {month}월 {day}일
+          My ToDo List - {year}년 {month}월 {day}일
         </Text>
       </View>
     </>
